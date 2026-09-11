5e17082c04bd00421a83d496e57eb42eeb82100f
Matt-poke <matthias.duchene@gmail.com>
2026-09-11 22:17:31 +0200

    Test filtre par royaume

--- api/gear-test.js
+++ api/gear-test.js
@@ -1,21 +1,26 @@
 import { graphql } from "./_wcl.js";
 
 export default async function handler(req, res) {
-  const { name, server, region } = req.query;
+  const { server, region } = req.query;
   try {
     const data = await graphql(
       `
-        query($name: String!, $server: String!, $region: String!) {
-          characterData {
-            character(name: $name, serverSlug: $server, serverRegion: $region) {
-              id
-              name
-              gearItems: gear
+        query($server: String!, $region: String!) {
+          worldData {
+            encounter(id: 12993) {
+              characterRankings(
+                className: "Mage"
+                specName: "Arcane"
+                metric: playerscore
+                serverSlug: $server
+                serverRegion: $region
+                includeCombatantInfo: true
+              )
             }
           }
         }
       `,
-      { name, server, region }
+      { server, region }
     );
     res.status(200).json(data);
   } catch (err) {
