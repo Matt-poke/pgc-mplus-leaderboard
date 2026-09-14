8a30bb8eaf5b1079dfa60f02d94c701f0b038758
Matt-poke <matthias.duchene@gmail.com>
2026-09-14 20:52:12 +0200

    ajout donjon altar of fangs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { NavLink, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Compare from "./pages/Compare.jsx";
+import Dungeons from "./pages/Dungeons.jsx";
+import DungeonDetail from "./pages/DungeonDetail.jsx";
 import "./App.css";
 
 export default function App() {
@@ -15,12 +17,17 @@ export default function App() {
           <NavLink to="/comparer" className={({ isActive }) => (isActive ? "active" : "")}>
             Comparer mon personnage
           </NavLink>
+          <NavLink to="/donjons" className={({ isActive }) => (isActive ? "active" : "")}>
+            Donjons
+          </NavLink>
         </div>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/comparer" element={<Compare />} />
+        <Route path="/donjons" element={<Dungeons />} />
+        <Route path="/donjons/:slug" element={<DungeonDetail />} />
       </Routes>
     </div>
   );
